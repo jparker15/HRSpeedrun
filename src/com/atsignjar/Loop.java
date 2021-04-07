1f85c9e88dada79644ef639d411a2507b27f71e7
Jeremy <jeremyeparker@yahoo.com>
2021-04-07 16:11:04 -0400

    print multiples

--- src/com/atsignjar/Loop.java
+++ src/com/atsignjar/Loop.java
@@ -1,4 +1,35 @@
 package com.atsignjar;
+import java.io.*;
+import java.util.*;
+
 
 public class Loop {
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        int cases = scanner.nextInt();
+        String s = "";
+
+        for(int j = 0; j < cases; j++){
+            s = scanner.next();
+            String even = "";
+            String odd= "";
+
+            for(int i = 0; i < s.length(); i++){
+                if(i % 2 == 0){
+                    even += s.charAt(i);
+
+                }else{
+                    odd += s.charAt(i);
+                }
+            }
+
+            System.out.println(even + " " + odd);
+        }
+
+
+    }
 }
