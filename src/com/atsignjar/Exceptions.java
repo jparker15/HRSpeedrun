e050ddf5ef47f7781fc07d3fe61780cc60acf9ea
Jeremy <jeremyeparker@yahoo.com>
2021-04-10 21:28:29 -0400

    try/catch

---
+++ src/com/atsignjar/Exceptions.java
@@ -0,0 +1,16 @@
+package com.atsignjar;
+import java.util.*;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+
+        try {
+            int i = Integer.parseInt(S);
+            System.out.println(i);
+        }catch(Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+}
